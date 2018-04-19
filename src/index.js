070f03b5ef5d1366cd0be7d3a4faa89459598f41
Mark Wallace <mark.wallace6@gmail.com>
2018-04-19 15:29:52 -0500

    changed function components to class components. getting results from youtube search

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyD3cuYtzqiokNuc0cDtTDSidCi-SmJ0czw';
 
-// Create a new component. This component should produce some html
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
-}
 
-// Take this component's HTML and put it on the page (in the DOM)
+class App extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
